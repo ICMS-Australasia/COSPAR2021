06151f13b99a2fe0524ab3ada95c5558811584b9
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-09-21 16:23:57 +1000

    Updated home banner background

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include('include/header.php');
           <div class="col-md p-0">
 
             <div class="embed-responsive embed-responsive-16by9">
-              <video controls autoplay muted>
+              <video controls autoplay>
                 <source src="./cospar-hybrid-final.mp4" type="video/mp4">
               </video>
             </div>
