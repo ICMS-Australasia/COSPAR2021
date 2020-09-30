f7ea1b722829680c29646069bac4bb58188e58d1
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-09-30 14:48:25 +1000

    Updates to sponsorship to virtual/hybrid

--- sponsorship-and-exhibition.php
+++ sponsorship-and-exhibition.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Sponsorship & Exhibition';
+$title = 'Virtual Platform Partnership Opportunities';
 include('include/header.php');
 ?>
         <!-- title -->
@@ -19,22 +19,26 @@ include('include/header.php');
                 <div class="row py-5">
                     <div class="col">
 
-                      <a href="https://icmsaust.eventsair.com/ExhibitionPortal/cospar-2021/exhibition/floorplan/live" class="button-main mb-4" target="_blank"><i class="fas fa-map-marker-alt"></i>View the Live Exhibition Floor Plan</a>
+                      <!-- <a href="https://icmsaust.eventsair.com/ExhibitionPortal/cospar-2021/exhibition/floorplan/live" class="button-main mb-4" target="_blank"><i class="fas fa-map-marker-alt"></i>View the Live Exhibition Floor Plan</a> -->
 
-                      <table cellpadding="10" class="mb-4">
-                          <tr>
-                              <td><a href="COSPAR2021-Floorplan.pdf" target="_blank"><img src="img/PDF-floorplan.png"></a></td>
-                              <td><a href="COSPAR2021-Floorplan.pdf" target="_blank">Download the COSPAR 2021 Exhibition Floor Plan</a></td>
-                          </tr>
-                      </table>
+
+                      <h2>Calling all sponsors and exhibitors to COSPAR-2021-Hybrid</h2>
 
-                        <p>Industry, government agencies, non-governmental groups and research organisations are all encouraged to participate in the exhibition at COSPAR 2021. With the objective of encouraging broad involvement by the global space sector, the exhibition will be incorporated as the centre piece of the 43rd COSPAR Assembly. Secure your presence in the exhibition and contact us today!</p>
-                        <p>For further information or to discuss how you could become involved as a sponsor or exhibitor, contact the COSPAR 2021 Organiser, ICMS Australasia:</p>
-                        <h3>Contact</h3>
-                        <p><b>COSPAR 2021 Event Director</b> <br>
-                            Emma Bowyer <br>
-                            ICMS Australasia <br>
-                            <a href="mailto:emmab@icmsaust.com.au">emmab@icmsaust.com.au</a></p>
+                        <p>Are you interested in getting your organisation profile out there? COSPAR-2021-Hybrid Virtual offers Sponsorship and Exhibition opportunities to connect you with a galaxy of networks.  Over 3,300 space researchers already signed up; if it’s about space it will be at COSPAR-2021-Hybrid.</p>
+                        <ul>
+                            <li>Giving sponsors and exhibitors the opportunity to present high-quality product demonstrations</li>
+                            <li>Offering new opportunities for sponsorships of special sessions</li>
+                            <li>3D exhibition booths to enhance promotion through a virtual market-place with real-time analytics and metrics</li>
+                            <li>Networking and meetings via live meeting hubs.</li>
+                        </ul>
+                        <table cellpadding="10" class="mb-4">
+                            <tr>
+                                <td><a target="_blank"><img src="img/virtual-prospectus.png" width="100"></a></td>
+                                <td><a target="_blank"><em>COSPAR-2021-Hybrid Prospectus Coming Soon!</em></a></td>
+                            </tr>
+                        </table>
+                        <p>For further information or to discuss how you could become involved as a sponsor or exhibitor, contact Emma Bowyer, COSPAR-2021-Hybrid, Event Director.  <br>
+                            <b>Email</b>: <a href="mailto:sponsorship@cospar2021.org">sponsorship@cospar2021.org</a></p>
 
                             <h2 class="mt-5 mb-3">Thank you to our Sponsors and Exhibitors</h2>
                             <h3 class="mt-5 mb-3">Hosted by</h3>
