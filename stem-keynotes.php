1b6eed09851781916c6dcc859f2fbf6880307a3a
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-10-21 15:29:04 +1100

    reordered keynote speakers

--- stem-keynotes.php
+++ stem-keynotes.php
@@ -13,7 +13,38 @@ include('include/cospark.php');
 
                       <h1 class="mt-3 mb-5">Keynotes</h1>
                       <p>COSPAR-K will feature a Keynote Speaker every lunch break to highlight the importance of STEM and why a career in space STEM is a future to consider. Be inspired, be challenged and be the future of Space STEM.</p>
-
+
+                      <h2 class="mt-5 mb-3">Phil Bland</h2>
+                      <div class="row my-4 align-items-center">
+                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
+                             <img src="img/cospark/phil-bland.jpg" class="img-fluid">
+                          </div>
+                          <div class="col-md mb-3 mb-md-0 align-items-center">
+                            <p>Phil Bland is Professor of Space & Planetary Science at Curtin University. He is Director of the Space Science and Technology Centre (SSTC). With 50 students and staff, it is the largest planetary research group in the southern hemisphere. He came to Australia on an ARC Laureate Fellowship in 2012. His research is focused on the origin and evolution of the solar system; how our planet formed; how it acquired the ingredients for life. He has been on multiple planetary mission science teams. In 2006, Asteroid ‘1981 EW21’ was renamed ‘(6580) Philbland’ in recognition of his contributions to space and planetary science. Major projects include the Desert Fireball Network (now expanding into a global facility), which was complemented by the multi-award winning Fireballs in the Sky outreach and citizen science program. In 2015 Bland established a partnership between NASA and Australia in planetary, space and exploration science. Curtin now formally represents the Australian planetary community to NASA. His team have been partnering with Lockheed Martin since 2016, translating DFN technology into applications in space situational awareness, and are now focussing on spacecraft engineering.</p>
+                            <p>For all budding planetary geologists or anyone wanting to know how to get an asteroid named after them, Phil’s talk is one to add to the calendar now.</p>
+                          </div>
+                      </div>
+                      <h2 class="mt-5 mb-3">Christyl Johnson</h2>
+                      <div class="row my-4 align-items-center">
+                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
+                             <img src="img/cospark/christyl-johnson.jpg" class="img-fluid">
+                          </div>
+                          <div class="col-md mb-3 mb-md-0 align-items-center">
+                            <p>Dr. Christyl Johnson is the Deputy Director for Technology and Research Investments at NASA Goddard Space Flight Centre. As NASA Goddard's Deputy Center Director for Technology and Research Investments, Christyl manages the Center's research and development portfolio and is responsible for formulating the Center's future science and technology goals and leading an integrated program of investments aligned to meet those goals.</p>
+                            <p>Christyl previously worked at the White House Office of Science and Technology Policy, where she served under the President's Science Advisor as the Executive Director of the National Science and Technology Council (NSTC). She was responsible for ensuring the establishment of clear national goals for Federal science and technology investments in a broad array of areas across the executive branch, including basic science, technology, energy, environment, natural resources and homeland and national security.</p>
+                            <p>Tune into Christyl’s talk and get inspired by one of the world’s foremost experts on space travel.</p>
+                          </div>
+                      </div>
+                      <h2 class="mt-5 mb-3">Russell Boyce</h2>
+                      <div class="row my-4 align-items-center">
+                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
+                             <img src="img/cospark/russell-boyce.jpg" class="img-fluid">
+                          </div>
+                          <div class="col-md mb-3 mb-md-0 align-items-center">
+                            <p>Professor Russell Boyce, COSPAR 2021 Chair and Chair for Intelligent Space Systems and Director of UNSW Canberra Space - Australia’s largest and leading space mission, research and education team. On the one hand, he has built and led teams pursuing game changing technologies across a three-decade career, firstly in hypersonics and scramjets and now in the development of artificial-intelligence-enabled space systems. He played a significant role in the establishment of the Australian Space Agency and is Australia’s only living Fellow of the American Institute of Aeronautics and Astronautics. On the other hand, he is a graduate of INSEAD’s flagship Advanced Management Program and of the Australian Institute of Company Directors and is passionate about the art of strategic yet personal leadership in an increasingly complex world.</p>
+                            <p>Bookmark Russ’ talk and hear directly from a renowned Australian leader in the space community.</p>
+                          </div>
+                      </div>
                       <h2 class="mt-5 mb-3">Corey Tutt</h2>
                       <div class="row my-4 align-items-center">
                           <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
@@ -47,37 +78,6 @@ include('include/cospark.php');
                               <p>Prior to Saber, Andreas created and ran a start-up, Obelisk Systems which developed next-generation CubeSat hardware, both for educational and commercial environments. This led to the development of the STEM education platform StarLAB which was a platform catered to high school students, ensuring they received the best in coding, hardware, sensing and robotics education to skill them for the rapidly evolving world. His presentation is a must-see for all aspiring Astronautic Engineers!</p>
                           </div>
                       </div>
-                      <h2 class="mt-5 mb-3">Phil Bland</h2>
-                      <div class="row my-4 align-items-center">
-                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
-                             <img src="img/cospark/phil-bland.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md mb-3 mb-md-0 align-items-center">
-                            <p>Phil Bland is Professor of Space & Planetary Science at Curtin University. He is Director of the Space Science and Technology Centre (SSTC). With 50 students and staff, it is the largest planetary research group in the southern hemisphere. He came to Australia on an ARC Laureate Fellowship in 2012. His research is focused on the origin and evolution of the solar system; how our planet formed; how it acquired the ingredients for life. He has been on multiple planetary mission science teams. In 2006, Asteroid ‘1981 EW21’ was renamed ‘(6580) Philbland’ in recognition of his contributions to space and planetary science. Major projects include the Desert Fireball Network (now expanding into a global facility), which was complemented by the multi-award winning Fireballs in the Sky outreach and citizen science program. In 2015 Bland established a partnership between NASA and Australia in planetary, space and exploration science. Curtin now formally represents the Australian planetary community to NASA. His team have been partnering with Lockheed Martin since 2016, translating DFN technology into applications in space situational awareness, and are now focussing on spacecraft engineering.</p>
-                            <p>For all budding planetary geologists or anyone wanting to know how to get an asteroid named after them, Phil’s talk is one to add to the calendar now.</p>
-                          </div>
-                      </div>
-                      <h2 class="mt-5 mb-3">Christyl Johnson</h2>
-                      <div class="row my-4 align-items-center">
-                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
-                             <img src="img/cospark/christyl-johnson.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md mb-3 mb-md-0 align-items-center">
-                            <p>Dr. Christyl Johnson is the Deputy Director for Technology and Research Investments at NASA Goddard Space Flight Centre. As NASA Goddard's Deputy Center Director for Technology and Research Investments, Christyl manages the Center's research and development portfolio and is responsible for formulating the Center's future science and technology goals and leading an integrated program of investments aligned to meet those goals.</p>
-                            <p>Christyl previously worked at the White House Office of Science and Technology Policy, where she served under the President's Science Advisor as the Executive Director of the National Science and Technology Council (NSTC). She was responsible for ensuring the establishment of clear national goals for Federal science and technology investments in a broad array of areas across the executive branch, including basic science, technology, energy, environment, natural resources and homeland and national security.</p>
-                            <p>Tune into Christyl’s talk and get inspired by one of the world’s foremost experts on space travel.</p>
-                          </div>
-                      </div>
-                      <h2 class="mt-5 mb-3">Russell Boyce</h2>
-                      <div class="row my-4 align-items-center">
-                          <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
-                             <img src="img/cospark/russell-boyce.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md mb-3 mb-md-0 align-items-center">
-                            <p>Professor Russell Boyce, COSPAR 2021 Chair and Chair for Intelligent Space Systems and Director of UNSW Canberra Space - Australia’s largest and leading space mission, research and education team. On the one hand, he has built and led teams pursuing game changing technologies across a three-decade career, firstly in hypersonics and scramjets and now in the development of artificial-intelligence-enabled space systems. He played a significant role in the establishment of the Australian Space Agency and is Australia’s only living Fellow of the American Institute of Aeronautics and Astronautics. On the other hand, he is a graduate of INSEAD’s flagship Advanced Management Program and of the Australian Institute of Company Directors and is passionate about the art of strategic yet personal leadership in an increasingly complex world.</p>
-                            <p>Bookmark Russ’ talk and hear directly from a renowned Australian leader in the space community.</p>
-                          </div>
-                      </div>
 
 
                       <h3 class="mt-5">More Speakers to Be Announced Soon!</h3>
