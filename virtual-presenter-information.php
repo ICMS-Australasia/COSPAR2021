f502e19e563c8171061d94c034cbc3ac1cd670fb
blairhardaker <blairhardaker@gmail.com>
2020-10-27 16:24:45 +1100

    Updated virtual presenters page and styles

--- virtual-presenter-information.php
+++ virtual-presenter-information.php
@@ -1,61 +1,72 @@
-<?php
-$title = 'Virtual Presenter Information';
-include('include/header.php');
-?>
-        <!-- title -->
-        <div class="title">
-            <div class="container">
-                <div class="row py-5">
-                    <div class="col py-4">
-                        <h1><?php echo $title; ?></h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- title -->
-        <!-- content -->
-        <div class="content">
-            <div class="container">
-                <div class="row py-5">
-                    <div class="col">
-
-                      <h2>Participation in COSPAR-2021-Hybrid: How will it work?</h2>
-                      <h3>COSPAR-2021-Hybrid will be held from 28 January 2021 to 4 February 2021 and will be Hybrid – a blend of in-person in Sydney, Australia and virtual (both interactive and on-demand).</h3>
-
-                      <h4>COSPAR-2021-Hybrid, a true global Assembly</h4>
-                      <p>The Scientific Program for COSPAR-2021-Hybrid is the program for COSPAR 2020, shifted from August to 28 January until 4 February, the new dates of COSPAR-2021-Hybrid. The only difference, as explained below, is how the program will be offered, so that COSPAR-2021-Hybrid will be a truly global Assembly, with full participation in multiple time zones. </p>
-
-                      <p>The details below are intended to provide the required information to everyone associated with the Scientific Program for COSPAR-2021-Hybrid: the authors of all accepted abstracts, the Main Scientific Organizers (MSOs) of all scientific events, the Chairs of Scientific Commissions and Panels, and the COSPAR Program Committee.  These include instructions of how the Scientific Program for COSPAR-2021-Hybrid will be conducted, of your opportunities and responsibilities for participating in the Scientific Program and of your opportunity for a reduced registration rate in return for on-time delivery of pre-recorded presentations.  </p>
-
-                      <h4>Event Schedule</h4>
-                      <p>There is no change in the program (themes, sessions, layout), now that COSPAR-2021 is COSPAR-2021-Hybrid.  </p>
-
-                      <p>All presentations will be pre-recorded by presenters, with the duration consistent as per the original program timings.  These will need to be uploaded for inclusion in the virtual conference delivery platform and will be available at the beginning of the Assembly, searchable according to COSPAR Commissions and Panels, technical themes, and keywords, to registered delegates only.</p>
-
-                      <h4>When will the final program be available</h4>
-                      <p>For the overall program and details of your Scientific event, please consult the Assembly website at:
-                        <a href="https://www.cospar-assembly.org" target="_blank">https://www.cospar-assembly.org</a>.</p>
-
-                      <p>With the latest call for abstracts now closed and under review, the final program will be available to view by 31 October.</p>
-
-                      <h4>Time zones</h4>
-                      <p>The program schedule is designed to deliver the full program across global time zones.  Each pre-recorded presentation will be scheduled to play twice.</p>
-
-                      <p>The program for each day will be concentrated into a three-hour block early in the Australian/Asian day (suitable for the American time zones), and repeated again at the end of the Australian/Asian day and into the evening (suitable for the European, African, and Middle-Eastern time zones). In this way, all time zones will be catered for.
-                        Outside these times, the presentations will be available on-demand until 31 December 2021.</p>
-
-                      <h4>Registration</h4>
-                      <p>All presenters are reminded they must register before <strong>30 November 2020</strong> as this is the Presenter Registration Deadline.  In order to be eligible for the early bird presenter rate, all the above documentation needs to be submitted on or before 30 November 2020.</p>
-
-                      <h4>Virtual Platform </h4>
-                      <p>Further information including a tutorial video will be sent to you in due course, regarding how to access the COSPAR-2021-Hybrid Virtual Platform including instructions for joining your session, managing live Q&A and more! </p>
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- content -->
-            <?php
-            include('include/footer.php');
-            ?>
+<?php
+$title = 'Virtual Presenter Information';
+include('include/header.php');
+?>
+<!-- title -->
+<div class="title">
+  <div class="container">
+    <div class="row py-5">
+      <div class="col py-4">
+        <h1><?php echo $title; ?></h1>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- title -->
+<!-- content -->
+<div class="content">
+  <div class="container">
+    <div class="row py-5 presenter-info">
+      <div class="col">
+        <h2>Participation in COSPAR-2021-Hybrid: How will it work?</h2>
+        <h3 class="mt-3 mb-3">COSPAR-2021-Hybrid will be held from 28 January 2021 to 4 February 2021 and will be Hybrid – a blend of in-person in Sydney, Australia and virtual (both interactive and on-demand).</h3>
+        <h2 class="mt-5 mb-3">COSPAR-2021-Hybrid, a true global Assembly</h2>
+        <p>The Scientific Program for COSPAR-2021-Hybrid is the program for COSPAR 2020, shifted from August to 28 January until 4 February, the new dates of COSPAR-2021-Hybrid. The only difference, as explained below, is how the program will be
+          offered, so that COSPAR-2021-Hybrid will be a truly global Assembly, with full participation in multiple time zones. </p>
+        <p>The details below are intended to provide the required information to everyone associated with the Scientific Program for COSPAR-2021-Hybrid: the authors of all accepted abstracts, the Main Scientific Organizers (MSOs) of all scientific
+          events, the Chairs of Scientific Commissions and Panels, and the COSPAR Program Committee. These include instructions of how the Scientific Program for COSPAR-2021-Hybrid will be conducted, of your opportunities and responsibilities for
+          participating in the Scientific Program and of your opportunity for a reduced registration rate in return for on-time delivery of pre-recorded presentations. </p>
+        <h2 class="mt-5 mb-3">Event Schedule</h2>
+        <p>There is no change in the program (themes, sessions, layout), now that COSPAR-2021 is COSPAR-2021-Hybrid. </p>
+        <p>All presentations will be pre-recorded by presenters, with the duration consistent as per the original program timings. These will need to be uploaded for inclusion in the virtual conference delivery platform and will be available at the
+          beginning of the Assembly, searchable according to COSPAR Commissions and Panels, technical themes, and keywords, to registered delegates only.</p>
+        <h2 class="mt-5 mb-3">When will the final program be available</h2>
+        <p>For the overall program and details of your Scientific event, please consult the Assembly website at: <a href="https://www.cospar-assembly.org" target="_blank">https://www.cospar-assembly.org</a>.</p>
+        <p>With the latest call for abstracts now closed and under review, the final program will be available to view by 31 October.</p>
+        <h2 class="mt-5 mb-3">Time zones</h2>
+        <p>The program schedule is designed to deliver the full program across global time zones. Each pre-recorded presentation will be scheduled to play twice.</p>
+        <p>The program for each day will be concentrated into a three-hour block early in the Australian/Asian day (suitable for the American time zones), and repeated again at the end of the Australian/Asian day and into the evening (suitable for
+          the European, African, and Middle-Eastern time zones). In this way, all time zones will be catered for. Outside these times, the presentations will be available on-demand until 31 December 2021.</p>
+        <h2 class="mt-5 mb-3">Registration</h2>
+        <p>All presenters are reminded they must register before <strong>30 November 2020</strong> as this is the Presenter Registration Deadline. In order to be eligible for the early bird presenter rate, all the above documentation needs to be
+          submitted on or before 30 November 2020.</p>
+        <h2 class="mt-5 mb-3">Virtual Platform </h2>
+        <p>Further information including a tutorial video will be sent to you in due course, regarding how to access the COSPAR-2021-Hybrid Virtual Platform including instructions for joining your session, managing live Q&A and more! </p>
+      </div>
+      <div class="col-md-3">
+        <div class="row presenter-cards">
+          <a href="#" class="col-md-12">
+            <img src="https://via.placeholder.com/300x200" alt="">
+            <div>Pre-recorded presentation guidelines</div>
+          </a>
+          <a href="#" class="col-md-12">
+            <img src="https://via.placeholder.com/300x200" alt="">
+            <div>Framing, lighting and recording guidelines</div>
+          </a>
+          <a href="#" class="col-md-12">
+            <img src="https://via.placeholder.com/300x200" alt="">
+            <div>System configuration requirements</div>
+          </a>
+          <a href="#" class="col-md-12">
+            <img src="https://via.placeholder.com/300x200" alt="">
+            <div>Technical support</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- content -->
+<?php
+include('include/footer.php');
+?>
