8ad5312d2af78131302891e2d07c0e8f2f21f4b3
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-10-28 12:06:54 +1100

    Updated profiles of a few speakers

--- stem-keynotes.php
+++ stem-keynotes.php
@@ -51,21 +51,21 @@ include('include/cospark.php');
                              <img src="img/cospark/corey-tutt.jpg" class="img-fluid">
                           </div>
                           <div class="col-md mb-3 mb-md-0 align-items-center">
-                              <p>Deadly Science Founder & Indigenous Australian Advocate, the 2020 NSW Young Australian of the Year Corey Tutt, a Kamilaroi man originally from Dapto in the Illawarra is the latest spectacular addition to the COSPAR-K Keynote Speaker line up.</p>
-                              <p>With an avid interest in science, Corey started the Deadly Science initiative in 2018 and has incredibly distributed over 11,000 books, 480 telescopes plus other many other resources to over 105 schools with no plan of slowing down soon.</p>
-                              <p>Deadly Science aims to provide early reading materials, science books and connect young Indigenous people with mentors to encourage their participation in STEM subjects across all of Australia, no matter how remote. Deadly Science wants to ensure all schools have access to our history of science by providing First Nations resources to connect back to our First Scientists; after all Indigenous Australian’s were the first Astronomers and are the oldest continuing culture in the world.</p>
-                              <p>We can’t wait to hear Corey share his story and inspire the next generation of STEM superstars.</p>
+                            <p>Deadly Science Founder & Indigenous Australian Advocate, the 2020 NSW Young Australian of the Year Corey Tutt, a Kamilaroi man originally from Dapto in the Illawarra confirmed as a Keynote Speaker for COSPAR-K.</p>
+                            <p>With an avid interest in science, Corey started the Deadly Science initiative in 2018 and has incredibly distributed over 11,000 books, 480 telescopes plus other many other resources to over 105 schools with no plan of slowing down soon.</p>
+                            <p>Deadly Science aims to provide early reading materials, science books and connect young Indigenous people with mentors to encourage their participation in STEM subjects across all of Australia, no matter how remote. Deadly Science wants to ensure all schools have access to our history of science by providing First Nations resources to connect back to our First Scientists; after all Indigenous Australian’s were the first Astronomers and are the oldest continuing culture in the world.</p>
+                            <p>We can’t wait to hear Corey share his story and inspire the next generation of STEM superstars.</p>
                           </div>
                       </div>
-                      <h2 class="mt-5 mb-3">Professor Lisa Harvey-Smith </h2>
+                      <h2 class="mt-5 mb-3">Lisa Harvey-Smith</h2>
                       <div class="row my-4 align-items-center">
                           <div class="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center">
                              <img src="img/cospark/lisa-harvey.png" class="img-fluid">
                           </div>
                           <div class="col-md mb-3 mb-md-0 align-items-center">
-                             <p>What a way to kick off our Keynote Speaker line-up! Professor Lisa Harvey-Smith is an award-winning astrophysicist with a talent for making the secrets of the universe accessible to all. She has spent 15 years conducting astrophysics research at universities and research institutes across the world.</p>
-                             <p>She is a member of the Advisory Group to the Australian Space Agency, a Professor of Practice at UNSW Sydney and the inaugural Australian Government Women in STEM Ambassador. The Ambassador’s role is to increase the participation of women and girls in Science, Technology, Engineering and Mathematics (STEM) studies and careers by building visibility and driving change towards gender equity in STEM.</p>
-                             <p>Professor Harvey-Smith has a talent for presenting complex subject matter in an exciting and accessible way so this is certainly a presentation not to be missed, especially for women and girls considering a career in STEM.</p>
+                            <p>Professor Lisa Harvey-Smith is an award-winning astrophysicist with a talent for making the secrets of the universe accessible to all. She has spent 15 years conducting astrophysics research at universities and research institutes across the world.</p>
+                            <p>She is a member of the Advisory Group to the Australian Space Agency, a Professor of Practice at UNSW Sydney and the inaugural Australian Government Women in STEM Ambassador. The Ambassador’s role is to increase the participation of women and girls in Science, Technology, Engineering and Mathematics (STEM) studies and careers by building visibility and driving change towards gender equity in STEM.</p>
+                            <p>Lisa has a talent for presenting complex subject matter in an exciting and accessible way so this is certainly a presentation not to be missed, especially for women and girls considering a career in STEM.</p>
                           </div>
                       </div>
                       <h2 class="mt-5 mb-3">Andreas Antoniades</h2>
@@ -74,8 +74,9 @@ include('include/cospark.php');
                              <img src="img/cospark/andreas-antoniades.jpg" class="img-fluid">
                           </div>
                           <div class="col-md mb-3 mb-md-0 align-items-center">
-                              <p>Welcoming Andreas Antoniades as the second Keynote Speaker to be confirmed for COSPAR-K. Andreas is passionate about developing engineering solutions to real-world problems, particularly for space and ensuring that these solutions are available and affordable to all. He is the Director of Saber Astronautics Australia and is kept busy with space mission operations, ground stations, democratisation of space and open source hardware, as well as currently building the nation's space Mission Control Centre, in Adelaide, SA.</p>
-                              <p>Prior to Saber, Andreas created and ran a start-up, Obelisk Systems which developed next-generation CubeSat hardware, both for educational and commercial environments. This led to the development of the STEM education platform StarLAB which was a platform catered to high school students, ensuring they received the best in coding, hardware, sensing and robotics education to skill them for the rapidly evolving world. His presentation is a must-see for all aspiring Astronautic Engineers!</p>
+                            <p>Andreas is passionate about developing engineering solutions to real-world problems, particularly for space and ensuring that these solutions are available and affordable to all. He is the Director of Saber Astronautics Australia and is kept busy with space mission operations, ground stations, democratisation of space and open source hardware, as well as currently building the nation's space Mission Control Centre, in Adelaide, SA.</p>
+                            <p>Prior to Saber, Andreas created and ran a start-up, Obelisk Systems which developed next-generation CubeSat hardware, both for educational and commercial environments. This led to the development of the STEM education platform StarLAB which was a platform catered to high school students, ensuring they received the best in coding, hardware, sensing and robotics education to skill them for the rapidly evolving world.</p>
+                            <p>His presentation is a must-see for all aspiring Astronautic Engineers!</p>
                           </div>
                       </div>
 
