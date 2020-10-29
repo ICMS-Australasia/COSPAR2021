f2582784a842934c5792b7eaae1cf33500d6a4b5
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-10-29 15:34:57 +1100

    Removed #top from links

--- pagination.js
+++ pagination.js
@@ -5,22 +5,22 @@ let pageButtons = ``;
 if (pageNum - Math.ceil(range/2) < 0) {
   console.log("Case 1");
   for (let i = 0; i < range; i ++) {
-    pageButtons += `<a href='./stem-competition-submissions.php#top?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+    pageButtons += `<a href='./stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
   }
-  pageButtons += `<a href='./stem-competition-submissions.php#top?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
+  pageButtons += `<a href='./stem-competition-submissions.php?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
 } else if (pageNum + Math.ceil(range/2) > pageArr.length) {
   console.log("Case 2");
-  pageButtons += `<a href='./stem-competition-submissions.php#top?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
+  pageButtons += `<a href='./stem-competition-submissions.php?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
   for (let i = pageArr.length - 5; i < pageArr.length; i ++) {
-    pageButtons += `<a href='./stem-competition-submissions.php#top?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+    pageButtons += `<a href='./stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
   }
 } else {
   console.log("Case 3");
-  pageButtons += `<a href='./stem-competition-submissions.php#top?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
+  pageButtons += `<a href='./stem-competition-submissions.php?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
   for (let i = pageNum - 3; i < pageNum + 2; i ++) {
-    pageButtons += `<a href='./stem-competition-submissions.php#top?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+    pageButtons += `<a href='./stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
   }
-  pageButtons += `<a href='./stem-competition-submissions.php#top?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
+  pageButtons += `<a href='./stem-competition-submissions.php?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
 }
 
 document.getElementById('submission-buttons').innerHTML = pageButtons;
