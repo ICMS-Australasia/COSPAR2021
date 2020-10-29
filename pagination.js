a61d8151c9cb12b566cbc743a7d872da1cb4795a
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-10-29 15:07:15 +1100

    Testing pagination buttons

---
+++ pagination.js
@@ -0,0 +1,35 @@
+let pageArr = [];
+for (let i = 0; i < submissions.length; i += 20) {
+  pageArr.push(submissions.slice(i, i + 20));
+}
+
+let queries = window.location.search;
+let parameters = new URLSearchParams(queries);
+
+let pageNum = parseInt(parameters.get('page'));
+let range = 5;
+console.log(`Page Number: ${pageNum}`);
+
+let pageButtons = ``;
+if (pageNum - Math.ceil(range/2) < 0) {
+  console.log("Case 1");
+  for (let i = 0; i < range; i ++) {
+    pageButtons += `<a href='/stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+  }
+  pageButtons += `<a href='/stem-competition-submissions.php?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
+} else if (pageNum + Math.ceil(range/2) > pageArr.length) {
+  console.log("Case 2");
+  pageButtons += `<a href='/stem-competition-submissions.php?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
+  for (let i = pageArr.length - 5; i < pageArr.length; i ++) {
+    pageButtons += `<a href='/stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+  }
+} else {
+  console.log("Case 3");
+  pageButtons += `<a href='/stem-competition-submissions.php?page=${pageNum - 1}' data-page="${pageNum - 1}" id="button${pageNum - 1}" class='col button'><i class="fas fa-chevron-left"></i></a>`
+  for (let i = pageNum - 3; i < pageNum + 2; i ++) {
+    pageButtons += `<a href='/stem-competition-submissions.php?page=${i+1}' data-page="${i+1}" id="button${i+1}" class='col button'>${i+1}</a>`
+  }
+  pageButtons += `<a href='/stem-competition-submissions.php?page=${pageNum + 1}' data-page="${pageNum + 1}" id="button${pageNum + 1}" class='col button'><i class="fas fa-chevron-right"></i></a>`
+}
+
+document.getElementById('submission-buttons').innerHTML = pageButtons;
--- stem-competition-submissions.php
+++ stem-competition-submissions.php
@@ -18,77 +18,78 @@ include('include/cospark.php');
                       <div class="row" id="submission-buttons"></div>
 
                       <script type="text/javascript" src="./competition-submissions.js"></script>
+                      <script type="text/javascript" src="./pagination.js"></script>
 
                       <script type="text/javascript">
-                        submissions = submissions.map((sub, index) =>
-                          `
-                          <a href="#submission-${index}" data-toggle="modal" target="_blank" class="col-12 col-sm-4 col-md-2 archive-item">
-                            <div class="archive-image">
-                              <img src="${sub.img}?text=${sub.title}" class="img-fluid" width="100%" loading="lazy">
-                            </div>
-                            <div class="archive-title">
-                                ${sub.title}
-                            </div>
-                          </a>
-                          <div class="submission-popups">
-                            <div class="modal fade" id="submission-${index}">
-                              <div class="modal-dialog modal-dialog-centered">
-                                <div class="modal-content">
-                                  <!-- modal content -->
-                                  <div class="row p-0 m-0">
-                                    <div class="col-md p-0 m-0">
-                                      <img src="${sub.img}" class="img-fluid" loading="lazy">
-                                    </div>
-                                    <div class="col-md-3 p-5 m-0 d-flex align-items-center">
-                                      <div>
-                                        <a type="button" class="close" data-dismiss="modal">&times;</a>
-                                        <span class="pop-title">${sub.title}</span>
-                                        <p class="pop-description">${sub.desc}</p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                                <!-- end modal content -->
-                              </div>
-                            </div>
-                          </div>
-                          `
-                        );
-
+                        // submissions = submissions.map((sub, index) =>
+                        //   `
+                        //   <a href="#submission-${index}" data-toggle="modal" target="_blank" class="col-4 col-sm-4 col-md-2 archive-item">
+                        //     <div class="archive-image">
+                        //       <img src="${sub.img}?text=${sub.title}" class="img-fluid" width="100%" loading="lazy">
+                        //     </div>
+                        //     <div class="archive-title">
+                        //         ${sub.title}
+                        //     </div>
+                        //   </a>
+                        //   <div class="submission-popups">
+                        //     <div class="modal fade" id="submission-${index}">
+                        //       <div class="modal-dialog modal-dialog-centered">
+                        //         <div class="modal-content">
+                        //           <!-- modal content -->
+                        //           <div class="row p-0 m-0">
+                        //             <div class="col-md p-0 m-0">
+                        //               <img src="${sub.img}" class="img-fluid" loading="lazy">
+                        //             </div>
+                        //             <div class="col-md-3 p-5 m-0 d-flex align-items-center">
+                        //               <div>
+                        //                 <a type="button" class="close" data-dismiss="modal">&times;</a>
+                        //                 <span class="pop-title">${sub.title}</span>
+                        //                 <p class="pop-description">${sub.desc}</p>
+                        //               </div>
+                        //             </div>
+                        //           </div>
+                        //         </div>
+                        //         <!-- end modal content -->
+                        //       </div>
+                        //     </div>
+                        //   </div>
+                        //   `
+                        // );
 
                         //Create groups of submissions
-                        let pageArr = [];
-                        for (let i = 0; i < submissions.length; i += 60) {
-                          pageArr.push(submissions.slice(i, i + 60));
-                        }
-                        pageArr = pageArr.map(p => p.join(``));
-                        pageArr.forEach((p, index) => {
-                          document.getElementById(`submissions`).innerHTML +=
-                            `
-                              <div id="page-${index}" class="page">
-                                <div class="row mb-4">
-                                  ${p}
-                                </div>
-                              </div>
-                            `
-                        });
-
-                        let btns = ``;
-                        btns = pageArr.map((page, index) =>
-                          `<button type="button" name="button${index+1}" id="button${index+1}" class='col mx-2 p-3' onclick="pagination(this); location.href='#top'">${index+1}</button>`
-                        ).join(``);
-                        document.getElementById('submission-buttons').innerHTML = btns;
-
-                        function pagination(e) {
-                          document.querySelectorAll(`div.page`).forEach(b => { b.classList.add(`d-none`); b.classList.remove(`d-block`); } );
-                          document.getElementById(`page-${e.innerHTML-1}`).classList.add(`d-block`);
-                          document.getElementById(`page-${e.innerHTML-1}`).classList.remove(`d-none`);
-                          document.querySelectorAll(`button`).forEach(b => { b.classList.remove(`active`); } );
-                          e.classList.add(`active`);
-                          window.scrollTo(0, 0);
-                        }
-
-                        pagination(document.getElementById("button1"));
+                        // let pageArr = [];
+                        // for (let i = 0; i < submissions.length; i += 20) {
+                        //   pageArr.push(submissions.slice(i, i + 20));
+                        // }
+                        // pageArr = pageArr.map(p => p.join(``));
+                        // pageArr.forEach((p, index) => {
+                        //   document.getElementById(`submissions`).innerHTML +=
+                        //     `
+                        //       <div id="page-${index}" class="page">
+                        //         <div class="row mb-4">
+                        //           ${p}
+                        //         </div>
+                        //       </div>
+                        //     `
+                        // });
+                        //
+                        // let btns = ``;
+                        // btns = pageArr.map((page, index) =>
+                        // `<button type="button" name="button${index+1}" data-page="${index}" id="button${index+1}" class='col-3' onclick="pagination(this); location.href='#top'">${index+1}</button>`
+                        //   `<a href='/stem-competition-submissions.php?page=${index+1}' data-page="${index+1}" id="button${index+1}" class='col-3 button'>${index+1}</a>`
+                        // ).join(``);
+                        // document.getElementById('submission-buttons').innerHTML = btns;
+                        //
+                        // function pagination(e) {
+                        //   document.querySelectorAll(`div.page`).forEach(b => { b.classList.add(`d-none`); b.classList.remove(`d-block`); } );
+                        //   document.getElementById(`page-${e.innerHTML-1}`).classList.add(`d-block`);
+                        //   document.getElementById(`page-${e.innerHTML-1}`).classList.remove(`d-none`);
+                        //   document.querySelectorAll(`button`).forEach(b => { b.classList.remove(`active`); } );
+                        //   e.classList.add(`active`);
+                        //   window.scrollTo(0, 0);
+                        // }
+                        //
+                        // pagination(document.getElementById("button1"));
                       </script>
                       <?php
                       include('include/cospark-footer.php');
