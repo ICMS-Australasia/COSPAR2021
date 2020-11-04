7ca37f7626eddfad9a6df6aaf326c40514028ddb
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-04 16:24:59 +1100

    Removed venue duplicate from header

--- include/header.php
+++ include/header.php
@@ -138,21 +138,18 @@
                                                 <a class="dropdown-item" href="technical-support">Technical Support</a>
                                             </div>
                                         </li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Venue
-                                                <i class="far fa-angle-down"></i>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item" href="venue.php">Venue</a>
-                                                <a class="dropdown-item" href="keeping-you-safe.php">Keeping you Safe</a>
-                                            </div>
-                                        </li>
                                         <li class="nav-item">
                                             <a class="nav-link" href="sponsorship-and-exhibition.php">Sponsorship</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="venue.php">Venue</a>
+                                        <li class="nav-item dropdown">
+                                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Venue
+                                            <i class="far fa-angle-down"></i>
+                                          </a>
+                                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="venue.php">Venue</a>
+                                            <a class="dropdown-item" href="keeping-you-safe.php">Keeping you Safe</a>
+                                          </div>
                                         </li>
                                         <!--li class="nav-item dropdown">
                                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
