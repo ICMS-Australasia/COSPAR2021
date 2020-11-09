fb09f4d82fe716681a4de077284c844ef140d8e8
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-09 15:09:51 +1100

    Edits to launch pad content and fixed link to ground map

--- stem.php
+++ stem.php
@@ -12,11 +12,11 @@ include('include/cospark.php');
         <h5>Get involved with COSPAR-K: A Free Space STEM Event which will feature two hero zones: our <b>Awesome Activities Arena</b> and <b>Spectacular Speakers Schedule</b>. We have something for everyone, whether you can attend in-person or would like to connect in virtually via COSPAR-K TV powered by Microsoft.</h5>
         <h3 class="my-4">The best part is tickets are 100% <b>FREE!</b></h3>
 
-        <a href="" class="button-main my-5">
+        <a href="./COSPAR-K-Ground-Map-Combined.pdf" target="_blank" class="button-main my-5">
           <span class="button-head">View the COSPAR-K Ground Map</span>
         </a>
 
-        <div class="row stem-cards mb-4">
+        <div class="row stem-cards justify-content-center mb-4">
           <!-- <a href="stem-about#cospark" class="col-md-6">
             <div class="stem-card">
               <img class="img-fluid" src="img/cospark/card1.jpg">
@@ -29,13 +29,12 @@ include('include/cospark.php');
               <h2 class="stem-card-title p-4" style="line-height: 1.5rem">Tickets <br> <span style="font-size:0.5em; font-weight: 400;"><em>All the ways you can get involved</em></span></h2>
             </div>
           </a>
-          <a href="stem-tickets#cospark" class="col-md-6">
+          <!-- <a href="stem-tickets#cospark" class="col-md-6">
             <div class="stem-card">
               <img class="img-fluid" src="https://via.placeholder.com/550x450">
-              <!-- <img class="img-fluid" src="img/cospark/card7.jpg"> -->
               <h2 class="stem-card-title p-4" style="line-height: 1.5rem">COSPAR-K TV <br> <span style="font-size:0.5em; font-weight: 400;"><em>powered by Microsoft</em></span></h2>
             </div>
-          </a>
+          </a> -->
           <!-- <a href="stem-competition#cospark" class="col-md-6">
             <div class="stem-card">
               <img class="img-fluid" src="img/cospark/card5.jpg">
@@ -45,7 +44,7 @@ include('include/cospark.php');
           <a href="stem-activities#cospark" class="col-md-6">
             <div class="stem-card">
               <img class="img-fluid" src="img/cospark/card2.jpg">
-              <h2 class="stem-card-title p-4">Awesome Activities Arena</h2>
+              <h2 class="stem-card-title p-4" style="line-height: 1.5rem">Awesome Activities Arena <br> <span style="font-size:0.5em; font-weight: 400;">&nbsp;</span></h2>
             </div>
           </a>
           <a href="stem-keynotes#cospark" class="col-md-6">
