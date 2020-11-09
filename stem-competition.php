6a03cff0674f64836dd45a5884429e1cfd8a4404
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-09 15:20:24 +1100

    Updated styles

--- stem-competition.php
+++ stem-competition.php
@@ -1,6 +1,6 @@
 <?php
 $cospark = true;
-$title = 'COSPAR-K Paint the Sky Competition';
+$title = 'COSPAR-K Lunar Classroom Competition';
 include('include/header.php');
 include('include/cospark.php');
 ?>
@@ -11,19 +11,19 @@ include('include/cospark.php');
                     <div class="col stem-page">
 
 
-                      <h1 class="mt-3 mb-5">Competitions</h1>
+                      <h1 class="mt-3 mb-4">COSPAR-K Lunar Classroom Competition</h1>
 
-                      <div class="alert alert-primary p-5 mb-5" style="background:#fbedfc">
-                        <h2 class="mt-0">COSPAR-K Lunar Classroom Competition</h2>
-                        <p>Calling all teachers on Planet Earth, we have a mission for you. Tell us what your teaching essentials are if you were tasked to set up the first ever classroom on the Moon!</p>
-                        <p>All your items must fit within a standard shoebox…apart from that the stars are your limit.</p>
-                        <p class="mb-0">Simply share your lunar classroom kit by uploading a photo or video to <b>Instagram</b>, <b>Facebook</b> or <b>Twitter</b> and tag <b>#cosparklunarclassoom</b> to be in the draw to WIN!</p>
-                      </div>
+                      <a class="button-secondary mb-5 p-5" style='width: 100%; color: white;'>
+                          <span class="button-head" style="text-decoration: none;">Calling all teachers on Planet Earth, we have a mission for you. </span>
+                          <span class="button-body">Tell us what your teaching essentials are if you were tasked to set up the first ever classroom on the Moon!</span>
+                          <span class="button-body">All your items must fit within a standard shoebox…apart from that the stars are your limit.</span>
+                          <span class="button-body mb-0">Simply share your lunar classroom kit by uploading a photo or video to <b>Instagram</b>, <b>Facebook</b> or <b>Twitter</b> and tag <b>#cosparklunarclassoom</b> to be in the draw to WIN!</span>
+                      </a>
                       <h2>The Design Kit Prize Pack</h2>
                       <p>We want to help your students create out of this world designs right here on Planet Earth. We also want you to preview one of our favourite activities that will be running at COSPAR-K - our FREE Space STEM Event.</p>
                       <p><a href="https://sispprogram.schools.nsw.gov.au/" target="_blank">The STEM Industry School Partnerships (SISP)</a> team are the geniuses behind our Plants vs Mars activity and have curated this incredible Design Kit Prize Pack to add to your ultimate classroom kit. We love that resources encourage creative design thinking, collaborative practices and an interdisciplinary (cross-curriculum) approach to solve complex problems.</p>
                       <p>There are <b>25 PRIZE PACKS</b> up for grabs, so what are you waiting for?</p>
-                      <p>Full prize inclusions are:</p>
+                      <h3>Full prize inclusions are:</h3>
                       <ul>
                         <li>10 x Plants vs Mars STEAM Activity Cards by the <a href="https://imaginaturalists.com.au/" target="_blank">Imaginaturalist</a></li>
                         <li>1 x Class Bundle of <a href="https://splat3d.com/shop/" target="_blank">Splats</a></li>
