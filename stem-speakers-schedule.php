4c382fa4cc4c9d256e843f7c93495b77f17c0f8a
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 14:32:34 +1100

    Reordered elements and layout

--- stem-speakers-schedule.php
+++ stem-speakers-schedule.php
@@ -13,17 +13,6 @@ include('include/cospark.php');
 
                       <h1 class="mt-3 mb-5">Spectacular Speakers Schedule</h1>
 
-                      <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-main mb-4 mt-5">
-                        <span class="button-head">Book Your Ticket Now</span>
-                        <span class="button-body">In-Person @ Pyrmont Theatre, ICC Sydney</span>
-                      </a>
-                      <!-- <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-secondary mb-4 mt-4">
-                        <span class="button-head mb-0">Virtual via COSPAR-K TV </span>
-                        <span class="button-body mt-0"><span style="font-size: 0.9em;"><b><em>powered by Microsoft</em></b></span> <br> Register Now</span>
-                      </a> -->
-                      <p>Our speaker line-up is truly amazing! We’ve curated this speaker program to highlight the importance of STEM and why a career in Space STEM is a future to seriously consider. We want you to be inspired, be challenged and be the future of Space STEM.</p>
-                      <p>Thanks to our super star partners Microsoft, no matter where you are on our beautiful Earth you can live-stream the entire COSPAR-K speakers program.</p>
-
                       <div class="row mt-5 speakers-schedule">
                         <a href="stem-keynotes" class=" col-12 col-sm-4 text-center">
                           <p class="icon m-0"><i class="fas fa-video"></i></p>
@@ -32,18 +21,37 @@ include('include/cospark.php');
                           <p>Mix of speakers appearing live on-stage and virtual via video. Keynotes will be recorded and available on-demand for convenient viewing.</p>
                         </a>
                         <a class="disabled col-12 col-sm-4 text-center">
-                          <p class="icon m-0"><i class="fas fa-comments"></i></p>
-                          <h3 class="mb-0">Power Talks</h3>
-                          <p><em>15 minutes</em></p>
-                          <p>Delivered in a mix of live on-stage and video presentations. Only available during the scheduled time.</p>
-                        </a>
-                        <a class="disabled col-12 col-sm-4 text-center">
                           <p class="icon m-0"><i class="fas fa-chalkboard-teacher"></i></p>
                           <h3 class="mb-0">Panel Discussions</h3>
                           <p><em>60 minutes</em></p>
                           <p>Featuring leaders in STEM education from both government and industry.</p>
                         </a>
+                        <a class="disabled col-12 col-sm-4 text-center">
+                          <p class="icon m-0"><i class="fas fa-comments"></i></p>
+                          <h3 class="mb-0">Power Talks</h3>
+                          <p><em>15 minutes</em></p>
+                          <p>Delivered in a mix of live on-stage and video presentations. Only available during the scheduled time.</p>
+                        </a>
                       </div>
+
+                      <div class="row my-4">
+                        <div class="col-md-6 col-12">
+                          <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-secondary mt-4 mt-md-0">
+                            <span class="button-head">Book Your In-Person Tickets Now!</span>
+                            <span class="button-body">@ Pyrmont Theatre, ICC Sydney</span>
+                          </a>
+                        </div>
+                        <div class="col-md-6 col-12">
+                          <a href="stem-tv" target="_blank" class="button-secondary mt-4 mt-md-0">
+                            <span class="button-head">COSPAR-K TV Bookings Opening Soon</span>
+                            <span class="button-body"><em>powered by Microsoft</em></b></span>
+                          </a>
+                        </div>
+                      </div>
+
+                      <p>Our speaker line-up is truly amazing! We’ve curated this speaker program to highlight the importance of STEM and why a career in Space STEM is a future to seriously consider. We want you to be inspired, be challenged and be the future of Space STEM.</p>
+                      <p>Thanks to our super star partners Microsoft, no matter where you are on our beautiful Earth you can live-stream the entire COSPAR-K speakers program.</p>
+
 
 
                       <?php
