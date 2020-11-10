d988a670122d863669f6a9a56c7f4c17df794d73
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 12:02:41 +1100

    Taken out stem-activities cards and made an include file for them to be on every activities page.

---
+++ include/stem-activities-cards.php
@@ -0,0 +1,44 @@
+<div class="row stem-cards mt-5">
+  <a href="./stem-activities-plantsvsmars.php" class="col-sm-6 col-12 col-md-3">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card1.jpg">
+      <h3 class="stem-card-title p-4">Plant vs Mars</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-robotrescue.php" class="col-sm-6 col-12 col-md-3">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card2.jpg">
+      <h3 class="stem-card-title p-4">Robot Rescue</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-droneracing.php" class="col-sm-6 col-12 col-md-3">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card3.jpg">
+      <h3 class="stem-card-title p-4">Drone Racing</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-satellitemission.php" class="col-sm-6 col-12 col-md-3">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card4.jpg">
+      <h3 class="stem-card-title p-4">Satellite Mission</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-giantmoon.php" class="col-sm-6 col-12 col-md-4">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card5.jpg">
+      <h3 class="stem-card-title p-4">Giant Moon & Mars Maps</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-marsandmonash.php" class="col-sm-6 col-12 col-md-4">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card6.jpg">
+      <h3 class="stem-card-title p-4">Mars Yard & Monash Nova Rover</h3>
+    </div>
+  </a>
+  <a href="./stem-activities-virtualreality.php" class="col-sm-6 col-12 col-md-4">
+    <div class="stem-card">
+      <img class="img-fluid" src="img/cospark/activity-card7.jpg">
+      <h3 class="stem-card-title p-4">Virtual Reality Helicopter Simulators</h3>
+    </div>
+  </a>
+</div>
