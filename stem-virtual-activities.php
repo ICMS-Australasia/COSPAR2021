1dce32caec99ebe550dab307a977cca1bc168c53
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 14:21:48 +1100

    Added Scavenger Hunt Event. Edited button

--- stem-virtual-activities.php
+++ stem-virtual-activities.php
@@ -16,10 +16,31 @@ include('include/cospark.php');
                       <p>To compliment all the in-person action, we’ve created virtual activities so you can continue the fun and learning from wherever you choose. Providing extended access and remote access to those not able to attend in-person due to location or other reasons has driven the team to develop these fun and educational experiences which are accessible to all.</p>
                       <p>Places are limited for some activities so book your tickets today to secure your spot.</p>
 
-                      <a href="" target="_blank" class="button-secondary mb-5 mt-5">
+                      <a style="color:white" target="_blank" class="button-secondary mb-5 mt-4">
                           <span class="button-head">Book your COSPAR-K TV Ticket Now</span>
                           <span class="button-body"><em>Powered by Microsoft</em></span>
                       </a>
+
+                      <div class="row mt-5 mb-3 d-flex align-items-end">
+                          <div class="col">
+                              <h2 class="mb-0">COSPAR-K Scavenger Hunt</h2>
+                          </div>
+                          <div class="col">
+                              <img src="./img/cospark/sisp.png" width="200" class="float-right">
+                          </div>
+                      </div>
+                      <div class="row my-4">
+                          <div class="col-md-12 mb-3 mb-md-0">
+                              <img src="img/cospark/scavenger-hunt-1.png" class="img-fluid">
+                          </div>
+                      </div>
+
+                      <p>We’ve developed an exciting on-line game for you to play during COSPAR-K. Play it from wherever you are; maybe you’re on the bus travelling to the ICC Sydney or lounging at home with some friends, this game will take you on an intergalactic adventure. Rough gaming time is 60 minutes. All you need is a device connected to the internet and you’re set. Choose from single player or a team of up to 10 people.</p>
+                      <p>Get your thinking hats on as Mars is waiting! There’s the plot:</p>
+                      <p>After years of training, the time has come for you to start the remarkable journey from Earth to Mars. Can you take off from Earth, navigate to the Moon and overcome cybersecurity threats along the way? After travelling millions of miles through space, can you then manage to establish human life on Mars?!</p>
+                      <p>This activity is available for all five days of COSPAR-K, book your COSPAR-K TV <em>powered by Microsoft</em> tickets to access this game.</p>
+
+
 
                       <?php
                       include('include/cospark-footer.php');
