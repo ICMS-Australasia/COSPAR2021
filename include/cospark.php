1db23e5ad1afb1ae8bf21b13636faac9de15fa20
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 13:38:08 +1100

    Added more items to the menu

--- include/cospark.php
+++ include/cospark.php
@@ -22,6 +22,9 @@
   <div class="stem-menu">
     <a href="stem#cospark">Launch Pad</a>
     <a href="stem-about">About COSPAR-K</a>
+    <a href="stem-tickets">Tickets</a>
+    <a href="stem-activities">Activities</a>
+    <a href="stem-speakers">Speakers</a>
     <a href="stem-competition">Competition</a>
     <a href="stem-catalogue">Media</a>
   </div>
