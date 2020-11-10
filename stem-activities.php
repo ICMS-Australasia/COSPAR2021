87d925b40880648237ca2f653a562a5618cf92b0
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 12:04:38 +1100

    Cards now lead into their own pages. Images included for each card

--- stem-activities.php
+++ stem-activities.php
@@ -1,5 +1,6 @@
 <?php
 $cospark = true;
+$home = false;
 $title = 'Activities';
 include('include/header.php');
 include('include/cospark.php');
@@ -17,200 +18,15 @@ include('include/cospark.php');
                       <p>Our activities are out of this world awesome! Check out the interactive experiences and activations specially designed for students, teachers and parents to get hands on and participate. Our incredible industry and government partners are providing opportunities for you to discover the latest innovations and also have fun through exploration.</p>
                       <p>COSPAR-K will be the space to ignite passion in our future thinkers and leaders so get involved and book your ticket today.</p>
                       <p>Click here to view the Arena Ground Map.</p>
-                      BOOK YOUR IN-PERSON TICKET NOW
 
                       <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-secondary mb-4 mt-5">
                           <span class="button-head">Book Your In-Person Ticket Now</span>
                       </a>
                       <!-- <p>Your COSPAR-K experience doesn’t end here, check out our <a href="stem-virtual-activities.php">Virtual Activities Program</a>.</p> -->
 
-
-                      <div class="row stem-cards mb-4 mt-5">
-                        <a href="./stem-activities.php?activity=PlantsVsMars" class="col-6 col-md-3">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card7.jpg">
-                            <h3 class="stem-card-title p-4">Plant vs Mars</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=RocketRescue" class="col-6 col-md-3">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card7.jpg">
-                            <h3 class="stem-card-title p-4">Rocket Rescue</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=DroneRacing" class="col-6 col-md-3">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card3.jpg">
-                            <h3 class="stem-card-title p-4">Drone Racing</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=SatelliteMission" class="col-6 col-md-3">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card3.jpg">
-                            <h3 class="stem-card-title p-4">Satellite Mission</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=GiantMoonAndMarsMaps" class="col-6 col-md-4">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card3.jpg">
-                            <h3 class="stem-card-title p-4">Giant Moon & Mars Maps <br> &nbsp;</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=MarsYardAndMonashNovaRover" class="col-6 col-md-4">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card2.jpg">
-                            <h3 class="stem-card-title p-4">Mars Yard & Monash Nova Rover</h3>
-                          </div>
-                        </a>
-                        <a href="./stem-activities.php?activity=VirtualRealityHelicopterSimulators" class="col-6 col-md-4">
-                          <div class="stem-card">
-                            <img class="img-fluid" src="img/cospark/card3.jpg">
-                            <h3 class="stem-card-title p-4">Virtual Reality Helicopter Simulators</h3>
-                          </div>
-                        </a>
-                      </div>
+                      <?php include('include/stem-activities-cards.php') ?>
 
 
-                      <!-- <div class="row mt-5 mb-3 align-items-end">
-                          <div class="col-7">
-                              <h2 class="mb-0">Monash Nova Rover</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/nova-rover-team.png" width="100" class="float-right">
-                          </div>
-                      </div>
-                      <div class="row my-4">
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/rover1.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/rover2.jpg" class="img-fluid">
-                          </div>
-                      </div>
-                      <p>Monash Nova Rover are a team of university students who design, fabricate and test the next generation of Mars rovers - inspiring future generations along the way. At COSPAR-K, we will be running an immersive activity where students can interact with our 2020 Mars Rover. They will complete tasks that actual Mars Rovers do and learn more about the science of space exploration and life detection. For those unable to attend in-person, we will be providing Virtual Excursions through DART Connections where students can access the program. Students will be able to engage with activities that challenge their critical thinking skills and engage them with interesting, interactive STEM ideas.</p>
-
-                      <hr class="my-5">
-
-                      <div class="row mt-5 mb-3 align-items-end">
-                          <div class="col-7">
-                              <h2 class="mb-0">Exploring Habitable Worlds with Praxical</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/praxical.png" width="100" class="float-right">
-                          </div>
-                      </div>
-                      <div class="row my-4">
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/habitable-worlds1.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/habitable-worlds2.jpg" class="img-fluid">
-                          </div>
-                      </div>
-                      <p>Have you ever wondered if there are aliens on other planets? In this workshop, you’ll learn what tools scientists use when looking for alien worlds. You'll use a NASA JPL planet simulator to explore how planet and star characteristics affect the planetary conditions needed for life. You will also learn about planets that break these rules and how scientists search for “life as we don't know it”. There are two options for this workshop, you can either do this workshop in your own time, or in a seminar led by two astrobiologists, where you get to ask them all your questions about the search for aliens!</p>
-                      <p>For a taste of what to expect, check out this video: </p>
-                      <div class="embed-responsive embed-responsive-16by9">
-                          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vzvBURvuiJs" allowfullscreen></iframe>
-                      </div>
-
-                      <hr class="my-5">
-
-                      <div class="row mt-5 mb-3 align-items-end">
-                          <div class="col">
-                              <h2 class="mb-0">UNSW Canberra Satellite Mission</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/unsw-canberra.jpg" width="100" class="float-right">
-                          </div>
-                      </div>
-                      <div class="row my-4">
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/satellite1.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/satellite2.jpg" class="img-fluid">
-                          </div>
-                      </div>
-                      <p>We’re excited to announce our latest awesome activity at COSPAR-K, the UNSW Canberra Satellite Mission!</p>
-                      <p>Design your very own mission and build the right satellite for it. Let your imagination run free and decide what your mission objectives are. Whether it’s looking for water on Mars, setting up a communications relay around the Moon or even monitoring bush fire behaviour on Earth. Any mission is possible; the challenge will be to configure the right satellite for the job.</p>
-
-                      <hr class="my-5">
-
-                      <div class="row mt-5 mb-3 d-flex align-items-end">
-                          <div class="col">
-                              <h2 class="mb-0">Plants vs Mars</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/sisp.png" width="200" class="float-right">
-                          </div>
-                      </div>
-                      <p>The team at NSW Department of Education STEM Industry School Partnerships Program will challenge you to create smart habitats to improve liveability in space for future Mars based communities. Inspired by NASA and Massachusetts Institute of Technology (MIT) Media’s Lab’s Space Exploration Initiative, you will learn to think like engineers in order to design Smart STEM focussed products for remote deployment and automation of devices on Mars. Areas of exploration will include product design, working with sensors and data, plus prototyping user interfaces for the remote management of resources.</p>
-
-                      <div class="row my-4">
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/plants-vs-mars-1.png" class="img-fluid">
-                          </div>
-                          <div class="col-md-6 mb-3 mb-md-0">
-                              <img src="img/cospark/plants-vs-mars-2.png" class="img-fluid">
-                          </div>
-                      </div>
-
-                      <div class="row mt-5 mb-3 align-items-end">
-                          <div class="col">
-                              <h2 class="mb-0">Drone Racing</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/auadrt.jpg" width="150" class="float-right">
-                          </div>
-                      </div>
-                      <p>Start your engines and get your adrenaline pumping by racing the Australian Army Drone Racing Team’s drones around their specially designed course. Learn and develop the skills involved with designing, building and maintaining drones whilst also getting your hands on a quality drone to race!</p>
-
-                      <div class="row my-4">
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/drone-racing-1.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/drone-racing-2.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/drone-racing-3.jpg" class="img-fluid">
-                          </div>
-                      </div>
-
-                      <hr class="my-5">
-
-                      <div class="row mt-5 mb-3 align-items-end">
-                          <div class="col">
-                              <h2 class="mb-0">Fly Army Virtual Reality Helicopter Simulators</h2>
-                          </div>
-                          <div class="col">
-                              <img src="./img/cospark/fly-army.jpg" width="150" class="float-right">
-                          </div>
-                      </div>
-                      <p>The Australian Army will be demonstrating its Fly Army Virtual Reality Helicopter Simulators with movement activated seats at COSPAR-K. They will be providing participants an opportunity to “fly” the simulators for themselves and get a taste of what it is like to fly a real helicopter. The Fly Army advocates will be available to discuss Aviation roles including Engineers, Pilots, Aircraft Trades, Ground Crew Roles and UAS (Drone) Operators.</p>
-
-                      <div class="row my-4">
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/helicopter-sim-1.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/helicopter-sim-2.jpg" class="img-fluid">
-                          </div>
-                          <div class="col-md-4 mb-3 mb-md-0">
-                              <img src="img/cospark/helicopter-sim-3.jpg" class="img-fluid">
-                          </div>
-                      </div>
-
-                      <hr class="my-5">
-
-                      <h2 class="mt-5 mb-3">And So Much More!</h2>
-                      <p>We’re busily working on bringing more awesome interactive experiences and activations to COSPAR-K. Stay glued to our communications for exciting announcements of the following activities in the coming months. <b>Did we mention this event was free?</b> Remember to register your <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-eoi/Site/Register" target="_blank">expression of interest</a> now to ensure you’re included with our key updates.  We’ll soon be making announcements about:</p>
-                      <ul>
-                          <li>Virtual Reality Experiences</li>
-                          <li>Bottle Rocket Competition</li>
-                          <li>Giant Moon Map Exploration</li>
-                        </ul> -->
-
                       <?php
                       include('include/cospark-footer.php');
                       ?>
