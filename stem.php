7f6d1975640a3d9083ca24c6e7f35d8f8271abe2
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 16:43:39 +1100

    Added microsoft and replaced on launch pad

--- stem.php
+++ stem.php
@@ -36,7 +36,7 @@ include('include/cospark.php');
           </a>
           <a href="stem-tv" class="col-md-6">
             <div class="stem-card">
-              <img class="img-fluid" src="img/cospark/card9.jpg">
+              <img class="img-fluid" src="img/cospark/card10.png">
               <h3 class="stem-card-title p-4" style="line-height: 1.5rem">COSPAR-K TV <br> <span style="font-size:0.7em; font-weight: 400;"><em>Powered by Microsoft</em></span></h3>
             </div>
           </a>
