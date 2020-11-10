74d4d582ce657c107e358a9af6db8f0dc7f36b8f
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-10 14:34:40 +1100

    Adding in buttons and reordered elements

--- stem-tickets.php
+++ stem-tickets.php
@@ -13,9 +13,9 @@ include('include/cospark.php');
 
                       <h1 class="mt-3 mb-5">Tickets</h1>
                       <!-- <img src="https://via.placeholder.com/1280x720" class="img-fluid mb-5"> -->
+                      <p>Get ready to blast off! Find out all the ways you can be involved at COSPAR-K below. Enjoy everything on offer by making the most of our week-long Free Space STEM Event.</p>
                       <img src="./img/cospark/tickets.jpg" class="img-fluid mb-5 w-100">
 
-                      <p>Get ready to blast off! Find out all the ways you can be involved at COSPAR-K below. Enjoy everything on offer by making the most of our week-long Free Space STEM Event.</p>
                       <h2>In-Person @ The ICC Sydney</h2>
                       <ul>
                         <li>Get hands-ons in our Awesome Activity Arena which is suitable for kids big and small.</li>
@@ -24,11 +24,22 @@ include('include/cospark.php');
                       </ul>
                       <p>Choose what you attend and when you attend via our booking system. There are 10 sessions to select from and you may book as an individual, public group or for a school excursion.</p>
                       <p><em>Sessions are currently capped at 150 people each due to COVID-19 restrictions.</em></p>
-                      <p>All in-person bookings include access to COSPAR-K TV <em>powered by Microsoft</em> allowing you to live stream the full speaker schedule during the week.</p>
-
-                      <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-secondary mb-5 mt-5">
-                          <span class="button-head">Book your In-Person Tickets Now!</span>
+                      <a href="https://icmsaust.eventsair.com/cospar-2021/cospar-k-tickets/Site/Register" target="_blank" class="button-main mb-5 mt-5">
+                        <span class="button-head">Book Your In-Person Tickets Now!</span>
                       </a>
+
+                      <h2>VIRTUAL via COSPAR-K TV <em>powered by Microsoft</em></h2>
+                      <ul>
+                        <li>Be inspired by our Spectacular Speakers covering topics for teachers, students and general space and science lovers.</li>
+                        <li>Experience our specially developed <a href="./stem-virtual-activities">virtual activities</a> from your classroom or at home.</li>
+                        <li>Make the most of the week-long festival of Space STEM by continuing the fun and learning wherever you are on our beautiful Earth.</li>
+                      </ul>
+
+                      <a href="stem-tv" target="_blank" class="button-secondary mb-5 mt-5">
+                        <span class="button-head mb-0">COSPAR-K TV Bookings Opening Soon</span>
+                        <span class="button-body my-0"><em>powered by Microsoft</em></span>
+                      </a>
+
 
                       <!-- <h2>VIRTUAL via COSPAR-K TV <span style="font-size: 0.7em;"><em>powered by Microsoft</em></span></h2>
                       <ul>
