60b1dee134800901b5e0b9fb8ebd94c15a656bbd
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-17 13:17:43 +1100

    Fixed Days to COSPAR 2021 countdown

--- include/footer.php
+++ include/footer.php
@@ -327,13 +327,13 @@ $(function () {
 
 
   // Set the date we're counting down to
-  var countDownDate = new Date("Nov 30, 2020 23:59:00").getTime();
+  var countDownDate2 = new Date("Nov 30, 2020 23:59:00").getTime();
   // Update the count down every 1 second
   var x = setInterval(function() {
     // Get today's date and time
     var now = new Date().getTime();
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    var distance = countDownDate2 - now;
     // Time calculations for days, hours, minutes and seconds
     var years = Math.floor(distance / (1000 * 60 * 60 * 24) / 365);
     var days = Math.floor(distance / (1000 * 60 * 60 * 24)) - years * 365;
