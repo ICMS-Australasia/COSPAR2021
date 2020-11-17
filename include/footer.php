2932b8be2ac1ef5b5e01fc72c0232821b6b4a4c0
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-18 09:13:37 +1100

    Merging with master branch

--- include/footer.php
+++ include/footer.php
@@ -327,21 +327,13 @@ $(function () {
 
 
   // Set the date we're counting down to
-<<<<<<< HEAD
-  var countDownDate = new Date("Nov 30, 2020 23:59:00").getTime();
-=======
   var countDownDate2 = new Date("Nov 30, 2020 23:59:00").getTime();
->>>>>>> 71c3e087ba3cc4110923d9f2b206d95cd52247cd
   // Update the count down every 1 second
   var x = setInterval(function() {
     // Get today's date and time
     var now = new Date().getTime();
     // Find the distance between now and the count down date
-<<<<<<< HEAD
-    var distance = countDownDate - now;
-=======
     var distance = countDownDate2 - now;
->>>>>>> 71c3e087ba3cc4110923d9f2b206d95cd52247cd
     // Time calculations for days, hours, minutes and seconds
     var years = Math.floor(distance / (1000 * 60 * 60 * 24) / 365);
     var days = Math.floor(distance / (1000 * 60 * 60 * 24)) - years * 365;
