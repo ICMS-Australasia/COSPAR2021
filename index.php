b47ca05760918ab58ef2a1bad25e7cb1c17ae053
blairhardaker <blairhardaker@gmail.com>
2020-11-19 16:34:10 +1100

    Addding Sixways to homepage

--- index.php
+++ index.php
@@ -1,93 +1,154 @@
-<?php
-$home = true;
-$title = '';
-include('include/header.php');
-?>
-<!-- <a href="abstracts.php" class="abstract-flash">
-    <div class="container">
-        <div class="row py-3">
-            <div class="col py-3">
-                <div class="opacity-change">Call for Abstracts Extended to 23 February 2020</div>
-            </div>
-        </div>
-    </div>
-</a> -->
-<!-- banner -->
-<div class="bigheader home">
-  <div class="container">
-      <div class="row align-items-center">
-          <div class="col-md-6">
-            <div id="abstract-countdown" class="mt-5 my-md-0 text-center"></div>
-          </div>
-          <div class="col-md-6">
-            <div class="embed-responsive embed-responsive-16by9 my-5">
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/lTrVbHfzkw4?modestbranding=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-
-          </div>
-      </div>
-  </div>
-</div>
-<!-- banner -->
-<!-- video -->
-<!--div class="bigheader home-video">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md p-0">
-
-              <div class="embed-responsive embed-responsive-16by9">
-                <video controls autoplay muted>
-                  <source src="img/fingertips.mp4" type="video/mp4">
-                Your browser does not support the video tag.
-                </video>
-              </div>
-
-            </div>
-        </div>
-    </div>
-</div-->
-<!-- video -->
-<!-- welcome -->
-<div class="container-fluid px-0">
-    <div class="row mx-0">
-        <div class="col-md-12 px-0">
+<?php
 
-          <div class="ticker">
-            <div class="ticker__list">
-              <div class="ticker__item">&bull; <a href="registration">Presenter access "presenter early bird rate" 30 November 2020</a></div>
-              <div class="ticker__item">&bull; <a href="presenter-information">Presenter deadline to upload pre-recorded short video 30 November 2020</a></div>
-              <div class="ticker__item">&bull; <a href="presenter-information">Presenter deadline to upload pre-recorded FULL video 31 December 2021</a></div>
+$home = true;
+
+$title = '';
+
+include('include/header.php');
+
+?>
+<!-- <a href="abstracts.php" class="abstract-flash">
+
+    <div class="container">
+
+        <div class="row py-3">
+
+            <div class="col py-3">
+
+                <div class="opacity-change">Call for Abstracts Extended to 23 February 2020</div>
+
             </div>
-          </div>
 
-          <script>
-          var ticker = document.querySelector(".ticker"),
-            list = document.querySelector(".ticker__list"),
-            clone = list.cloneNode(true);
+        </div>
 
-          ticker.append(clone);
-          </script>
+    </div>
 
-        </div>
-    </div>
+</a> -->
+<!-- banner -->
+<div class="bigheader home">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+        <div id="abstract-countdown" class="mt-5 my-md-0 text-center"></div>
+      </div>
+      <div class="col-md-6">
+        <div class="embed-responsive embed-responsive-16by9 my-5">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/lTrVbHfzkw4?modestbranding=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="welcome" id="invitation">
-  <div class="container">
-    <div class="row pt-5">
-      <div class="col-md-12 mt-2 mb-3 text-center">
-        <h2 class="mb-4" style="line-height:1.4; font-size:3em;" id="chair">From the Desk of the Chair</h2>
-        <!-- <h3 class="mb-5">News just in. COSPAR HQ have been inundated with requests for an extension. In the interest of having the best science at the largest space research conference; <br> <a href="abstracts.php" style="color:#0c4da2; text-decoration:underline;">COSPAR 2021 abstracts will remain open until 11 October 2020.</a></h3> -->
-      </div>
-      <div class="col-md-12 mb-3" style="font-size:1.1em;">
-
-        <img src="img/logo-co2.jpg" class="float-right ml-md-4 mb-md-4" width="160" alt="">
-
+<!-- banner -->
+<!-- video -->
+<!--div class="bigheader home-video">
+
+    <div class="container">
+
+        <div class="row align-items-center">
+
+            <div class="col-md p-0">
+
+
+
+              <div class="embed-responsive embed-responsive-16by9">
+
+                <video controls autoplay muted>
+
+                  <source src="img/fingertips.mp4" type="video/mp4">
+
+                Your browser does not support the video tag.
+
+                </video>
+
+              </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div-->
+<!-- video -->
+<!-- welcome -->
+<div class="container-fluid px-0">
+  <div class="row mx-0">
+    <div class="col-md-12 px-0">
+      <div class="ticker">
+        <div class="ticker__list">
+          <div class="ticker__item">&bull; <a href="registration">Presenter access "presenter early bird rate" 30 November 2020</a></div>
+          <div class="ticker__item">&bull; <a href="presenter-information">Presenter deadline to upload pre-recorded short video 30 November 2020</a></div>
+          <div class="ticker__item">&bull; <a href="presenter-information">Presenter deadline to upload pre-recorded FULL video 31 December 2021</a></div>
+        </div>
+      </div>
+      <script>
+        var ticker = document.querySelector(".ticker"),
+          list = document.querySelector(".ticker__list"),
+          clone = list.cloneNode(true);
+        ticker.append(clone);
+
+      </script>
+    </div>
+  </div>
+</div>
+<div class="sixways">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>6 Steps to Recording your Presentation Short Summary Video</h1>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="sixways_heading">1. Decide how you want to record your presentation</div> We suggest using Zoom with video and presentation tools. Using Zoom will allow you to the option to share your screen and sound, play videos and show
+            PowerPoints etc.
+          </div>
+          <div class="col-md-6">
+            <div class="sixways_heading">2. Prepare your presentation content</div>
+            Select your method of recording depending on whether you have a PowerPoint or video component to your presentation
+          </div>
+          <div class="col-md-6">
+            <div class="sixways_heading">3. Prepare to record your presentation</div>
+            We suggest checking your computer settings and using a headset with microphone
+          </div>
+          <div class="col-md-6">
+            <div class="sixways_heading">4. Record your short summary presentation video</div>
+          </div>
+          <div class="col-md-6">
+            <div class="sixways_heading">5. Submitting your pre-recorded presentation</div>
+            Upload and save your files to a file sharing service (dropbox or wetransfer or equivalent) so they can be sent through the presentation portal
+          </div>
+          <div class="col-md-6">
+            <div class="sixways_heading">6. Register through the presenter portal that has been emailed directly to you</div>
+            Please make contact with <a href="mailto:enquiries@cospar2021.org">enquiries@cospar2021.org</a> if you do not have your login details
+          </div>
+        </div>
+        <p>See the <a href="https://cospar2021.org/pre-record-guidelines ">full instruction guidelines here</a>.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="welcome" id="invitation">
+  <div class="container">
+    <div class="row pt-5">
+      <div class="col-md-12 mt-2 mb-3 text-center">
+        <h2 class="mb-4" style="line-height:1.4; font-size:3em;" id="chair">From the Desk of the Chair</h2>
+        <!-- <h3 class="mb-5">News just in. COSPAR HQ have been inundated with requests for an extension. In the interest of having the best science at the largest space research conference; <br> <a href="abstracts.php" style="color:#0c4da2; text-decoration:underline;">COSPAR 2021 abstracts will remain open until 11 October 2020.</a></h3>
+ -->
+      </div>
+      <div class="col-md-12 mb-3" style="font-size:1.1em;">
+        <img src="img/logo-co2.jpg" class="float-right ml-md-4 mb-md-4" width="160" alt="">
         <p>This edition of From the Desk of the Chair, comes via video format, uniting myself and Dr Len Fisk, the President of COSPAR in an open conversation about COSPAR-2021-Hybrid.</p>
-        <p>In the video, we share with you what is in store for you at COSPAR-2021-Hybrid. Some months ago, we set out to deliver an event that will represent the state-of-the-art for how to bring global communities together in the face of the pandemic - and with our investment in a bespoke virtual platform technology for the event, we are succeeding!</p>
-        <p>No matter where you are in the world and at a time of day that works for you: you will get access to ALL content, pre-recorded, until the end of 2021; you’ll participate in live-streamed Q&A in each session and for each keynote talk and panel; you’ll have live access to exhibitors in the virtual Exhibit Hall, as well as real-time virtual access to talent; you’ll get to participate in the parallel program of special events and workshops we’re calling INTERSECT (where space meets …); and perhaps most importantly, you’ll be able to engage in virtual yet real-time corridor conversations, the moments that start new friendships, build partnerships, create opportunities, that has been sorely missed this year. The technology and the production of an assembly of this size and sophistication do not come for free and our commitment to COSPAR’s critical financial support is firm. Nevertheless, we have worked hard to reduce your registrations fees. The result, we believe, is quite amazing value for money and will help stimulate our global collaborative efforts in this new world we have found ourselves.</p>
+        <p>In the video, we share with you what is in store for you at COSPAR-2021-Hybrid. Some months ago, we set out to deliver an event that will represent the state-of-the-art for how to bring global communities together in the face of the
+          pandemic - and with our investment in a bespoke virtual platform technology for the event, we are succeeding!</p>
+        <p>No matter where you are in the world and at a time of day that works for you: you will get access to ALL content, pre-recorded, until the end of 2021; you’ll participate in live-streamed Q&A in each session and for each keynote talk and
+          panel; you’ll have live access to exhibitors in the virtual Exhibit Hall, as well as real-time virtual access to talent; you’ll get to participate in the parallel program of special events and workshops we’re calling INTERSECT (where space
+          meets …); and perhaps most importantly, you’ll be able to engage in virtual yet real-time corridor conversations, the moments that start new friendships, build partnerships, create opportunities, that has been sorely missed this year. The
+          technology and the production of an assembly of this size and sophistication do not come for free and our commitment to COSPAR’s critical financial support is firm. Nevertheless, we have worked hard to reduce your registrations fees. The
+          result, we believe, is quite amazing value for money and will help stimulate our global collaborative efforts in this new world we have found ourselves.</p>
         <p>Thank you for your confidence and support!</p>
-
-
         <div class="row mt-4 mb-3">
           <div class="col-md-6 mb-3 mt-3 chair">
             <div class="row ml-2 ">
@@ -115,15 +176,11 @@ include('include/header.php');
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
+</div> <?php
 
-
-      </div>
-
-
-    </div>
-
-  </div>
-</div>
-<?php
-        include('include/footer.php');
-        ?>
+        include('include/footer.php');
+
+        ?>
