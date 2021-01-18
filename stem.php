dbe985fc2d09b5f0ffcc08e12cd9edc0ccb7c28a
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-01-18 15:16:24 +1100

    Updated stem page

--- stem.php
+++ stem.php
@@ -9,6 +9,15 @@ include('include/cospark.php');
   <div class="container">
     <div class="row py-5">
       <div class="col stem-page">
+        <div class="alert alert-primary p-4">
+          <img src="./img/cospark/COSPAR-K-postponement.png" class="img-fluid w-50 ml-0 ml-md-3 mb-4 d-none d-md-block float-right">
+          <img src="./img/cospark/COSPAR-K-postponement.png" class="img-fluid d-block d-sm-block d-md-none mb-4">
+          <p>In light of the reinstatement of border closures with NSW and quarantine periods, as a result for those leaving NSW, COSPAR-K Mission Control have decided to officially postpone our Free Space STEM Event to later in the year. We aim to run COSPAR-K in <b>August/September 2021</b> and for our Space STEM Event to have an affiliation with National Science Week 2021.</p>
+          <p>We will be sharing more information with you soon and look forward to welcoming you to our inaugural Space STEM Event in the future.</p>
+          <p>For the period between 29 Jan – 2 Feb 2021, we will be proceeding with our <a href="https://icmsaust.us11.list-manage.com/track/click?u=adaae520cf9dd252446148abc&id=f304cb8532&e=250e22b9b6" target="_blank">Virtual Activities</a>! We are working to add further opportunities for you to be able to participate in as a teaser event to the big show in August/September 2021.</p>
+          <p>We thank you for your ongoing support and trust that the above is welcomed news. Be sure that we will be in touch again shortly with further updates. We look forward to creating this event and meeting you in-person very soon!</p>
+          <p class="mb-0"><em><b>COSPAR-K Mission Control</b></em></p>
+        </div>
         <h1 class="text-center">Get involved at COSPAR-K</h1>
         <p class="mt-4 text-center">Our Free Space STEM Event which will feature two hero zones: our <a href="stem-activities" class="purple"><b>Awesome Activities Arena</b></a> and <a href="stem-speakers-schedule" class="purple"><b>Spectacular Speakers Schedule</b></a>. We have something for everyone, whether you can attend in-person or would like to connect in virtually via COSPAR-K TV <em>powered by Microsoft</em>.</p>
         <h3 class="my-4 text-center">The best part is tickets are 100% <b>FREE!</b></h3>
