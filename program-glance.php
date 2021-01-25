bbb54a6537fe55d74c9cf2beda045f14801ef9a3
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-01-25 13:52:54 +1100

    Program styles

--- program-glance.php
+++ program-glance.php
@@ -39,7 +39,7 @@ include('include/header.php');
                             </a>
 
                           </th>
-                          <th width="14.2%" class="border-white">
+                          <th width="7.5%" class="border-white">
                             <a href="/program-saturday.php" class="table-header justify-content-center">
                               <div><strong>Saturday <br> 30 Jan 2021</strong><br /><span>View Full Program</span></div>
                             </a>
@@ -54,7 +54,7 @@ include('include/header.php');
                             <div><strong>Tuesday <br> 2 Feb 2021</strong><br /><span>View Full Program</span></div>
                           </a>
                            </th>
-                          <th width="14.2%" class="border-white" colspan="2"><a href="/program-wednesday.php" class="table-header justify-content-center">
+                          <th width="7.5%" class="border-white" colspan="2"><a href="/program-wednesday.php" class="table-header justify-content-center">
                             <div><strong>Wednesday <br> 3 Feb 2021</strong><br /><span>View Full Program</span></div>
                           </a> </th>
                         </tr>
@@ -174,26 +174,26 @@ include('include/header.php');
                               AEDT 08:00 - 11:00</td>
                         </tr>
                         <tr>
-                          <td rowspan="3" class="border-white">
+                          <td rowspan="3" class="bg-teal-lighter border-white">
                             <b>
-                              Authentic Leadership Workshop <br><br>
+                              Authentic Leadership Workshop</b> <br><br>
                               Day One (AM) <br><br>
                               AEDT 10:00 - 13:00
-                            </b>
+
                           </td>
-                          <td rowspan="3" class="border-white">
+                          <td rowspan="3" class="bg-teal-lighter border-white">
                             <b>
-                              Authentic Leadership Workshop <br><br>
+                              Authentic Leadership Workshop</b> <br><br>
                               Day Two <br><br>
                               AEDT 10:00 - 13:00
-                            </b>
+
                           </td>
-                          <td rowspan="3" class="border-white">
+                          <td rowspan="3" class="bg-teal-lighter border-white">
                             <b>
-                              Authentic Leadership Workshop <br><br>
+                              Authentic Leadership Workshop</b> <br><br>
                               Day Three <br><br>
                               AEDT 10:00 - 13:00
-                            </b>
+
                           </td>
                         </tr>
                         <tr>
@@ -244,7 +244,7 @@ include('include/header.php');
                               11:00 - 12:30 AEDT
                             </b>
                           </td>
-                          <td class="bg-event-pink border-white">
+                          <td class="bg-stem border-white">
                             <b>
                               Public Lecture: <br>
                               12:00 - 13:10 AEDT <br>
@@ -285,8 +285,8 @@ include('include/header.php');
                           <td colspan="2" class="border-white"></td>
                         </tr>
                         <tr>
-                          <td rowspan="2" class='border-white'>
-                            Authentic Leadership Workshop Cont'd <br><br>
+                          <td rowspan="2" class='bg-teal-lighter border-white'>
+                            <b>Authentic Leadership Workshop Cont'd</b> <br><br>
                             Day One (PM) <br><br>
                             18:00 - 21:00 AEDT
                           </td>
@@ -296,8 +296,8 @@ include('include/header.php');
                             John Church <br>
                             <em>Australia</em> <br>
                             <u>Climate Change, the Ocean and Cryosphere</u></td>
-                          <td rowspan="2" class='border-white'>
-                            Authentic Leadership Workshop Cont'd <br><br>
+                          <td rowspan="2" class='bg-teal-lighter border-white'>
+                            <b>Authentic Leadership Workshop Cont'd</b> <br><br>
                             Day TWO (PM) <br><br>
                             18:00 - 21:00 AEDT
                           </td>
@@ -314,8 +314,8 @@ include('include/header.php');
                             Carlé McGetchin Pieters <br>
                             Brown University, USA <br>
                             The Earth-Moon System: Our Past, Present and Future</td>
-                          <td rowspan="2" class='border-white'>
-                            Authentic Leadership Workshop Cont'd <br><br>
+                          <td rowspan="2" class='bg-teal-lighter border-white'>
+                            <b>Authentic Leadership Workshop Cont'd</b> <br><br>
                             Day Three (PM) <br><br>
                             18:00 - 21:00 AEDT
                           </td>
@@ -389,204 +389,6 @@ include('include/header.php');
                           <td colspan="2" class="bg-S-lighter border-white">
                             <b>Virtual Posters</b></td>
                         </tr>
-                        <!-- <tr>
-                          <td class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 11:00 - 12:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 09:30 - 11:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 11:00 - 12:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 11:00 - 12:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 11:00 - 12:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 11:00 - 12:00
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td class="border-white">
-                            <b>In-Person Networking/Lunch</b> <br> AEDT 11:00 - 12:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Networking/Lunch</b> <br> AEDT 11:00 - 12:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Networking/Lunch</b> <br> AEDT 11:00 - 12:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Networking/Lunch</b> <br> AEDT 11:00 - 12:30
-                          </td>
-                          <td colspan="2" class="bg-event border-white">
-                            <b>Public Lecture: Abigail Allwood (USA)</b> <br>
-                            AEDT 12:00 - 13:30 <br> <br>
-                            Australian geologist and astrobiologist at the NASA Jet Propulsion Laboratory
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Networking/Lunch</b> <br> AEDT 11:00 - 12:30
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td colspan="11" class="bg-S-darker border-white">
-                            <b>INTERSECT Program</b> <br>
-                            AEDT 12:30 - 14:00
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                          <td colspan="2" class="border-white">
-                            <b>In-Person Afternoon Tea Break</b> <br> AEDT 14:00 - 14:30
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td colspan="11" class="bg-S-darker border-white">
-                            <b>INTERSECT Program</b> <br>
-                            AEDT 14:30 - 16:00
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td class="border-white"></td>
-                          <td colspan="2" class="bg-teal border-white">
-                            <b>Opening & Awards Ceremony</b> <br> AEDT 17:00 - 18:00 <br> Darling Harbour Theatre
-                          </td>
-                          <td colspan="2" class="border-white"></td>
-                          <td colspan="2" class="border-white"></td>
-                          <td colspan="2" class="border-white"></td>
-                          <td colspan="2" class="border-white"></td>
-                        </tr>
-                        <tr>
-                          <td class='border-white'></td>
-                          <td class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            John Church <br>
-                            University of New South Wales, Sydney, Australia <br>
-                            <u>Climate Change, the Ocean and Cryosphere</u></td>
-                          <td colspan="2" class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            Hitoshi Kuninaka
-                            Astronautical Science (ISAS), Japan Aerospace Exploration Agency (JAXA), Kanagawa, Japan, The Hayabusa II mission and return
-                            The Status of Hayabusa II and the Results thus far from Hayabusa I and II</td>
-                          <td colspan="2" class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            Carlé McGetchin Pieters <br>
-                            Brown University, USA <br>
-                            The Earth-Moon System: Our Past, Present and Future</td>
-                          <td colspan="2" class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            Michele Dougherty <br>
-                            Professor of Space Physics at Imperial College London (UK) <br>
-                            New Aspects of the Magnetospheres of Jupiter and Saturn</td>
-                          <td colspan="2" class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            Marica Branchesi <br>
-                            Gran Sasso Science Institute and INFN-LNGS, L'Aquila, Italy <br>
-                            New High Energy Astrophysics Involving Gravitational Waves and Satellite Observations</td>
-                          <td colspan="2" class="bg-S border-white">
-                            <b>Interdisciplinary Lecture</b> <br>
-                            AEDT 18:15 - 19:00 <br><br>
-                            Francis A. Cucinotta <br>
-                            University of Nevada, Las Vegas, USA <br>
-                            Radiobiology, Radiation and Why They Matter for Space Research and Exploration</td>
-                        </tr>
-                        <tr>
-                          <td class="border-white"></td>
-                          <td class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b>
-                            ORALS
-                            AEDT 19:00 - 22:00</td>
-                          <td rowspan="2" class="bg-teal border-white">
-                            <b>Welcome Party <br>
-                              AEDT 19:00 - 21:30</b> <br>
-                              Event Deck @ ICC</td>
-                          <td class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b> <br>
-                            ORALS <br>
-                            AEDT 19:00 - 20:30</td>
-                          <td colspan="2" class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b> <br>
-                            ORALS <br>
-                            AEDT 19:00 - 22:00</td>
-                          <td colspan="2" class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b> <br>
-                            ORALS <br>
-                            AEDT 19:00 - 22:00</td>
-                          <td colspan="2" class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b> <br>
-                            ORALS <br>
-                            AEDT 19:00 - 22:00</td>
-                          <td colspan="2" class="bg-S-light border-white">
-                            <b>BLOCK  2 <br>
-                              Virtual Scientific Sessions</b> <br>
-                            ORALS <br>
-                            AEDT 19:00 - 22:00</td>
-                        </tr>
-                        <tr>
-                          <td class="border-white"></td>
-                          <td class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 22:00 - 23:00
-                          </td>
-                          <td class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 20:30 - 21:30
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 22:00 - 23:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 22:00 - 23:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 22:00 - 23:00
-                          </td>
-                          <td colspan="2" class="bg-S-lighter border-white">
-                            <b>Virtual Posters</b> <br>
-                            AEDT 22:00 - 23:00
-                          </td>
-                        </tr> -->
                       </table>
 
 
